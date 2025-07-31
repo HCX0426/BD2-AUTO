e7c464eb7b02cbe149fac8ff24296be4ce1154c0
HCX0426 <chongxuan-huang@outlook.com>
2025-07-31 22:30:39 +0800

    优化模块导入，明确导出列表，添加邮件获取相关图片资源

--- auto_control/config/__init__.py
+++ auto_control/config/__init__.py
@@ -1,2 +1,7 @@
-from .control__config import *
-
+from .ocr_config import *
+from .log_config import *
+from .ocr_config import *
+from .image_config import *
+from .auto_config import *
+from .st_config import *
+from .task_config import *
--- auto_tasks/pc/__init__.py
+++ auto_tasks/pc/__init__.py
@@ -1,4 +1,8 @@
-from . import login
-from . import get_email
+# 显式导入模块中的函数
+from .get_email import get_email
+from .get_guild import get_guild
+from .get_restaurant import get_restaurant
+from .login import login
 
-__all__ = ['get_email', 'login']  # 显式导出模块
+# 明确导出列表
+__all__ = ['login', 'get_email', 'get_restaurant', 'get_guild']
